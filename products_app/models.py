daf97bc0d3cdef861809a88f21f487cb51bb7dba
Stuart Roeszler <stuart@roeszler.com>
2022-08-16 09:20:56 +0000

    add Category model to products_app > models.py

--- products_app/models.py
+++ products_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """ Model for the fixtures in products_app to go in """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)  # friendly name is optional
+
+    def __str__(self):
+        """ Takes in category model and returns name """
+        return self.name
+    
+    def get_friendly_name(self):
+        """ Takes in category model and returns the friendly_name """
+        return self.friendly_name
+
+class Product(models.Model):
+    """ 
+    Model for the fixtures in products_app to go in.
+    Foreign key to the category model above, null in DB and blank in forms.
+    if a category is deleted, any products that use it will to have null 
+    for category field rather than deleting the product.
+    """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
