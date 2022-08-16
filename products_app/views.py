ef63974547268418ae4f2cac6f46f73b94589148
Stuart Roeszler <stuart@roeszler.com>
2022-08-16 16:21:32 +0000

    fix product_id to product.id herf products.html

--- products_app/views.py
+++ products_app/views.py
@@ -18,4 +18,4 @@ def product_detail(request, product_id):
     context = {
         'product': product,
     }
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'products/product-detail.html', context)
