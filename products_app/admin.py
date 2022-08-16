9eab78b35b33ccc6ae471055904396672f94af5e
Stuart Roeszler <stuart@roeszler.com>
2022-08-16 12:57:55 +0000

    fix superuser mobile and add productAdmin Category adim models to alter admin page content

--- products_app/admin.py
+++ products_app/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    """ to extend the built in model admin class: product list view """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    # tuple to sort the products by SKU using the ordering attribute
+    ordering = ('sku', )
+
+class CategoryAdmin(admin.ModelAdmin):
+    """ to extend the built in model admin class: category list view """
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
