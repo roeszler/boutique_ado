27bfde6c3b0875add04977c9accdcf6226d1b825
Stuart Roeszler <stuart@roeszler.com>
2022-08-17 07:38:29 +0000

    add sorting function to views.py by all_products

--- products_app/views.py
+++ products_app/views.py
@@ -7,8 +7,10 @@ from .models import Product, Category
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
     products = Product.objects.all()
-    query = None  # ensures no error when loading without a search_term
-    categories = None  # ensures no error when loading without a search_term
+    query = None  # ensures no error when loading without a search term
+    categories = None  # ensures no error when loading without a categories filter
+    sort = None  # ensures no error when loading without a sort filter
+    direction = None  # ensures no error when loading without a direction filter
 
 # To return results where the query was matched in either the product name OR
 # the description. The OR logic is derived through 'Q'.
@@ -30,11 +32,32 @@ def all_products(request):
             categories = request.GET['category'].split(',')  # split into a list at the commas
             products = products.filter(category__name__in=categories)  # use list to filter current query to only products whose category 'name' is in the list.
             categories = Category.objects.filter(name__in=categories)  # filter all categories down to the ones whose name is in the list from the URL.
+        
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey  # sort into sortkey is to preserve the original field we want it to sort on name.
+            
+            # Now annotate all the products with a new field:
+            if sortkey == 'name':
+                sortkey = 'lower_name_annotation'
+                products = products.annotate(lower_name_annotation=Lower('name'))
+
+            # To check whether direction is descending
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'descending':  # if direction=descending...
+                    sortkey = f'-{sortkey}'  # using string formatting to reverse the order
+            
+            products = products.order_by(sortkey)  # to sort the products
+
+    # To return the current sorting methodology to the template
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
     return render(request, 'products/products.html', context)
 
