03005e505f78d0c320ff5cd3774c3d7d7e6db5cf
Stuart Roeszler <stuart@roeszler.com>
2022-08-17 06:04:25 +0000

    add request.GET query in all_products views

--- products_app/views.py
+++ products_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q  # object to generate a search query
 from .models import Product
 
 # Create your views here.
@@ -6,6 +8,18 @@ from .models import Product
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
     products = Product.objects.all()
+
+# To return results where the query was matched in either the product name OR
+# the description. The OR logic is derived through 'Q'.
+# If q is in request.get, set q to equal a variable called 'query'
+# Django messages framework & redirect, reversely back to the products url
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
     context = {
         'products': products,
     }
