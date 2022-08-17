4ab04f6cd51c1354a83cd7c21342a4df8971f565
Stuart Roeszler <stuart@roeszler.com>
2022-08-17 13:12:45 +0000

    add final product-detail.html code fix and add_to_bag view function in bag_app>views.py

--- bag_app/urls.py
+++ bag_app/urls.py
@@ -1,8 +1,7 @@
-# from django.contrib import admin
 from django.urls import path
-# import views from the current directory
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag')
+    path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
