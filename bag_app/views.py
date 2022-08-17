65883c2c10f54e6d18c8a306feebacda7efa0ec9
Stuart Roeszler <stuart@roeszler.com>
2022-08-17 13:29:16 +0000

    fadd add_to_bag functionality

--- bag_app/views.py
+++ bag_app/views.py
@@ -12,13 +12,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+
+    # session until client and server are done communicating
+    # allows shopping bag to persist until session is closed.
+    # Trys to get this variable if it already exists or 
+    # initializing it to an empty dictionary if it doesn't.
     bag = request.session.get('bag', {})
 
+    # can place the product and quantity in the bag dictionary {}
+    # and increment it by quantity orderd if it is already in the bag
     if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+        bag[item_id] += quantity 
     else:
         bag[item_id] = quantity
-
+    
+    # update the bag variable into the session [ a python dictionary ].
     request.session['bag'] = bag
-    print(request.session['bag'])
+    # print(request.session['bag'])  # to test
+
+    # Redirect the user back to the redirect URL
     return redirect(redirect_url)
+
