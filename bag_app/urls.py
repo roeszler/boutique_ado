2e140d9b927aa5728bc6236456f58c8d04bbf5e3
Stuart Roeszler <stuart@roeszler.com>
2022-08-18 13:27:53 +0000

    add view.py for adjust_bag and url.py and form action in bag.html

--- bag_app/urls.py
+++ bag_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
-    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'), # add_to_bag function in views.py
+    path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'), # adjust_bag function in views.py
 ]
