68a45e374f76a2074c7c4c6dfcc257ae34d8d45b
Stuart Roeszler <stuart@roeszler.com>
2022-08-19 12:28:08 +0000

    add cared entry error information field

--- checkout_app/static/checkout/js/stripe_elements.js
+++ checkout_app/static/checkout/js/stripe_elements.js
@@ -38,3 +38,20 @@ let card = elements.create('card', {style: style});
 
 // Mount the card to the div we created checkout.html
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+// with a handy little icon and error detail next to it:
+card.addEventListener('change', function(event) {
+    let errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        let html = `
+        <span class="icon" role="alert">
+            <i class="fas fa-times"></i>
+        </span>
+        <span>${event.error.message}</span>
+        `
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
