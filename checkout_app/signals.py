42ebf1dba57be3b0e3640b290896aa4c4c945b3f
Stuart Roeszler <stuart@roeszler.com>
2022-08-19 07:39:15 +0000

    add new signals module

---
+++ checkout_app/signals.py
@@ -0,0 +1,28 @@
+"""
+these signals are sent by django to the entire application
+after a model instance is saved and after it's deleted
+"""
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+# to execute this function anytime the post_save signal is sent
+# Receiving post_save signals from the OrderLineItem model:
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Special function to handle signals from the post_save event.
+    Update order total on lineitem update / create.
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, instance, deleted, **kwargs):
+    """
+    Special function to handle signals from the post_save event.
+    Update order total on lineitem delete.
+    """
+    instance.order.update_total()
