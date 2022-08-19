7dac4e4d4769681a1a3b1753ace4d578592cf512
Stuart Roeszler <stuart@roeszler.com>
2022-08-19 07:39:21 +0000

    add new signals module

--- checkout_app/apps.py
+++ checkout_app/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CheckoutAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout_app'
+
+    def ready(self):
+        """ Overriding the ready method and importing our signals module. """
+        import checkout_app.signals
