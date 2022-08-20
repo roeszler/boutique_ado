1a8809de6ce1092e94e96e9e13aabea7acf7c9e1
Stuart Roeszler <stuart@roeszler.com>
2022-08-20 06:07:00 +0000

    add checkout_success views.py

--- checkout_app/views.py
+++ checkout_app/views.py
@@ -124,3 +124,18 @@ def checkout_success(request, order_number):
     To take the order number and render success page
     indicating to the user that their payment is complete.
     """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
