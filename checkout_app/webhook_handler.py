d861039afd6aaa823561c5a3c71f6563e609df0f
Stuart Roeszler <stuart@roeszler.com>
2022-08-21 08:17:07 +0000

    add handled stripe web hooks

--- checkout_app/webhook_handler.py
+++ checkout_app/webhook_handler.py
@@ -15,7 +15,27 @@ class StripeWH_Handler:
         self.request = request
     
     def handle_event(self, event):
-        """ Handle a generic/unknown/unexpected webhook event """
+        """ 
+        Handle that listens for a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200
+        )
+    
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle listening for the payment_intent.succeeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle listening for the payment_intent.payment_failed webhook from Stripe
+        """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200
