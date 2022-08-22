320c4e4c83382191300d46d1540de64c0d4b36f2
Stuart Roeszler <stuart@roeszler.com>
2022-08-22 12:02:47 +0000

    fix example of django key for future use

--- boutique/settings.py
+++ boutique/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m6lj^95v%f)s0^2#t!)25j0s2svhe*k6@a5f%59g^(pnsm=o4c'
+SECRET_KEY = os.environ.get('SECRET_KEY_DJANGO', '')  # key with empty default value
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
