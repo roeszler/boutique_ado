15eb749e6d4da7223feb4d054c0573323dc50b9c
Stuart Roeszler <stuart@roeszler.com>
2022-08-22 18:57:23 +0000

    add handler for POST method to update delivery details

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -1,5 +1,6 @@
 """ Import Modules """
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -8,6 +9,14 @@ from .forms import UserProfileForm
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    # To handle the POST method to update delivery details in profile.html
+    # with the 'profile' we have just retrieved above.
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            message.success(request, 'Profile updated successfully')
 
     # To populate profile with the current users profile info:
     form = UserProfileForm(instance=profile)
