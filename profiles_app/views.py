75d9b492ebf64b0b583d0584a2b1911ab93b4a0b
Stuart Roeszler <stuart@roeszler.com>
2022-08-22 17:56:58 +0000

    add user profile form pages

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -2,14 +2,24 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    # To populate profile with the current users profile info:
+    form = UserProfileForm(instance=profile)
+
+    # To render an order history
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        # 'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
