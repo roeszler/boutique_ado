0e07827a82e0aae418769c27078cb063ff4074a4
Stuart Roeszler <stuart@roeszler.com>
2022-08-23 14:17:01 +0000

    WORKING VERSION restore from after dj_database_url

--- products_app/admin.py
+++ products_app/admin.py
@@ -1,5 +1,7 @@
+""" Import Modules """
 from django.contrib import admin
 from .models import Product, Category
+
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
@@ -16,12 +18,14 @@ class ProductAdmin(admin.ModelAdmin):
     # tuple to sort the products by SKU using the ordering attribute
     ordering = ('sku', )
 
+
 class CategoryAdmin(admin.ModelAdmin):
     """ to extend the built in model admin class: category list view """
     list_display = (
         'friendly_name',
         'name',
     )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
