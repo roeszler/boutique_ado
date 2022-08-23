b6ee727148945d2f6378408bbacc8cb3abee2298
Stuart Roeszler <stuart@roeszler.com>
2022-08-23 05:36:44 +0000

    add order history section in my profile page

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -33,3 +33,24 @@ def profile(request):
     }
 
     return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """
+    To render the past orders of the user 
+    """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    # Using the checkout success template
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True, # So we can check in that template if the user got there via the order history view
+    }
+
+    return render(request, template, context)
