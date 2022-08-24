21008a782fb64987c978bb66a3a605ace9423760
Stuart Roeszler <stuart@roeszler.com>
2022-08-24 19:09:23 +0000

    testing Server Error (500) from heroku

--- boutique/settings.py
+++ boutique/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')  # key with empty default value
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+# DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'boutique-ado-sr.herokuapp.com',
