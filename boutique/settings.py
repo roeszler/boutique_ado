f94caaed80afb233db5231e50dd252ce6bb0ebde
Stuart Roeszler <stuart@roeszler.com>
2023-01-30 08:35:33 +0000

    pre render readme settings

--- boutique/settings.py
+++ boutique/settings.py
@@ -209,7 +209,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Tuple to tell Django where all our static files are located
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
